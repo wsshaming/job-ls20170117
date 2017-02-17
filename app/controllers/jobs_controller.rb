f7adbdf3bcef20241e2b40f416f8ac742e9a4140
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-02-17 12:13:36 +0800

    implement groups#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,17 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:职缺, :岗位描述)
+  end
 end
