0bb5643ccc5d0ad3b743d03589b9f3060e676031
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-02-21 17:24:59 +0800

    use before_action to find_job_and_check_permission

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @jobs = Job.all
   end
@@ -9,10 +10,6 @@ class JobsController < ApplicationController
   end
 
   def edit
-    @job = Job.find(params[:id])
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def new
@@ -32,12 +29,6 @@ class JobsController < ApplicationController
  end
 
   def update
-    @job = Job.find(params[:id])
-
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     if @job.update(job_params)
       redirect_to jobs_path, notice: "修改 成功"
     else
@@ -46,18 +37,20 @@ class JobsController < ApplicationController
   end
 
   def destroy
-    @job = Job.find(params[:id])
-
-    if current_user != @job.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     @job.destroy
     flash[:alert] = "职缺 删除"
     redirect_to jobs_path
   end
 
   private
+
+  def find_job_and_check_permission
+    @job = Job.find(params[:id])
+
+  if current_user != @job.user
+    redirect_to root_path, alert: "You have no permission."
+  end
+end
 
   def job_params
     params.require(:job).permit(:职缺, :岗位描述)
