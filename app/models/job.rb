5e1513b815dd2f07707a0e4120fd31fc7265bea5
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-02-22 09:38:56 +0800

    add post and hook job/user relation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :user
+  has_many :posts
   validates :职缺, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :jobs
+  has_many :posts
 end
