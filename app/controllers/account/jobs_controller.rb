ca3a359630fbd7e9461e97f870407ce1d1d535ab
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-02-22 16:23:59 +0800

    implement my jobs

--- app/controllers/account/jobs_controller.rb
+++ app/controllers/account/jobs_controller.rb
@@ -1,2 +1,7 @@
 class Account::JobsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @jobs = current_user.participated_jobs
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  namespace :account do
+    resources :jobs
+  end
+
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
